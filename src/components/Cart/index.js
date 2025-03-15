cdcc72f65e7fd5b8f86c4b24e9fefa4561ec5711
saiTarun <guptasaitarun@gmail.com>
2025-03-15 12:59:33 +0000

    slove

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,17 +1,44 @@
 import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+import CartSummary from '../CartSummary'
+
 import './index.css'
 
 const Cart = () => (
-  <>
-    <Header />
-    <div className="cart-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
-        alt="cart"
-        className="cart-img"
-      />
-    </div>
-  </>
-)
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
 
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <div className="cart-header">
+                  <h1 className="cart-heading">My Cart</h1>
+                  <button
+                    type="button"
+                    className="remove-all-btn"
+                    onClick={removeAllCartItems}
+                  >
+                    Remove All
+                  </button>
+                </div>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
 export default Cart
